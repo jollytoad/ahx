742cff507b22ace81e7ce5a5fd48b2f17857c246
Mark Gibson <mgibson@adaptavist.com>
2025-04-10 15:28:10 +0100

    removing logging from dispatchToAll to break circular dep

--- common/dispatch-to-all.ts
+++ common/dispatch-to-all.ts
@@ -1,5 +1,4 @@
 import type { ActionResult } from "@ahx/types";
-import * as log from "@ahx/custom/log/event.ts";
 
 export function dispatchToAll<T extends EventTarget>(
   targets: T[] | undefined,
@@ -11,8 +10,6 @@ export function dispatchToAll<T extends EventTarget>(
 
   for (const target of targets) {
     const event = createEvent(target);
-
-    log.event(event, target);
 
     if (target.dispatchEvent(event) === false) {
       shouldBreak = true;
